3468542e1f199edb7a9c34df559c3640c6da3e4c
Alexander Shvedov <60114847+a-shvedov@users.noreply.github.com>
2023-05-17 14:42:27 +0300

    Update fuzz_lib.cpp

--- mayhem/fuzz_lib.cpp
+++ mayhem/fuzz_lib.cpp
@@ -1,14 +1,48 @@
 #include "fuzzer/FuzzedDataProvider.h"
-#include "csv.h"
+#include "../csv.h"
+#include <iostream>
+#include <sstream>
+#include <chrono>
+#include <thread>
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+enum class FuzzingErrorCode {
+    NoError,
+    InvalidInputSize,
+    LineReaderError,
+    CSVReaderError,
+    TimeoutError
+};
+
+void reportError(FuzzingErrorCode errorCode, const std::string& errorMessage) {
+    std::cerr << "Fuzzing error: " << static_cast<int>(errorCode) << " - " << errorMessage << std::endl;
+}
+
+// добавлена проверка size
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+    if (size < 2 ) {
+        reportError(FuzzingErrorCode::InvalidInputSize, "Размер входных данных не соответвует условиям");
+        return static_cast<int>(FuzzingErrorCode::InvalidInputSize);
+    }
+
     FuzzedDataProvider fdp(data, size);
-    try {
+
+    auto startTime = std::chrono::steady_clock::now();
+   try {
         if (fdp.ConsumeBool()) {
             // Test the LineReader
             auto buff = fdp.ConsumeRemainingBytesAsString();
             io::LineReader reader{"fuzz.in", buff.c_str(), buff.c_str() + buff.size()};
-            while (reader.next_line());
+            while (reader.next_line()) {
+                // Perform necessary processing on each line
+
+                // timer checker (default = 1)
+                auto currentTime = std::chrono::steady_clock::now();
+                auto elapsedSeconds = std::chrono::duration_cast<std::chrono::seconds>(currentTime - startTime).count();
+                if (elapsedSeconds > 1) {
+                    reportError(FuzzingErrorCode::TimeoutError, "Истечение таймера - пропуск семпла");
+                    return static_cast<int>(FuzzingErrorCode::TimeoutError);
+                }
+            }
         } else {
             // Test the CSVReader
             auto buff = fdp.ConsumeRemainingBytesAsString();
@@ -16,11 +50,25 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
             int w;
             std::string x, y;
             double z;
-            while (in.read_row(w, x, y, z));
+            while (in.read_row(w, x, y, z)) {
+                // проверка выполнения кода по таймеру условия elapsedSeconds
+                auto currentTime = std::chrono::steady_clock::now();
+                auto elapsedSeconds = std::chrono::duration_cast<std::chrono::seconds>(currentTime - startTime).count();
+                if (elapsedSeconds > 1) {
+                    reportError(FuzzingErrorCode::TimeoutError, "Истечение таймера - пропуск семпла");
+                    return static_cast<int>(FuzzingErrorCode::TimeoutError);
+                }
+            }
         }
-    } catch (const io::error::base &e) {
-        return -1;
+    } catch (const io::error::base& e) {
+        if (fdp.ConsumeBool()) {
+            reportError(FuzzingErrorCode::LineReaderError, e.what());
+            return static_cast<int>(FuzzingErrorCode::LineReaderError);
+        } else {
+            reportError(FuzzingErrorCode::CSVReaderError, e.what());
+            return static_cast<int>(FuzzingErrorCode::CSVReaderError);
+        }
     }
 
-    return 0;
+    return static_cast<int>(FuzzingErrorCode::NoError);
 }
